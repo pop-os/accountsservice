da50d797059a7eba0cf1c264e41a76493e328b32
Alessio Treglia <alessio@debian.org>
2011-01-30 01:47:14 +0100

    Imported Upstream version 0.6.2

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,10 @@
-. gnome-autogen.sh
+#!/bin/sh
+
+ACLOCAL="aclocal $ACLOCAL_FLAGS"
+export ACLOCAL
+
+(cd $(dirname $0);
+ autoreconf --install --force --symlink --verbose &&
+ intltoolize --force &&
+ autoreconf --force --verbose &&
+ ./configure $@)
